2f733b69f49fdd70e55e11809817bb2e64bc8c8f
yuHFUT <3209310348@qq.com>
2026-06-20 19:29:10 +0800

    Deploy

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'sleep-cbti-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
