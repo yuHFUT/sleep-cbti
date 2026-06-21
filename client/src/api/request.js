b3e18eaee7f546a41d54b913e335ee6871ad7963
yuHFUT <3209310348@qq.com>
2026-06-21 18:19:02 +0800

    Auto-update: https://old-singers-learn.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://beige-garlics-lie.loca.lt/api';
+  return 'https://old-singers-learn.loca.lt/api';
 }
 
 const request = axios.create({
