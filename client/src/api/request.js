d6910905a142fb487c866a354d5848c9192617fd
yuHFUT <3209310348@qq.com>
2026-06-21 18:20:04 +0800

    Auto-update: https://gold-goats-film.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://all-apes-leave.loca.lt/api';
+  return 'https://gold-goats-film.loca.lt/api';
 }
 
 const request = axios.create({
