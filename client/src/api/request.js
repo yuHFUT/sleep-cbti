58d1e2f1662103905e040d9aab4924d85fea607a
yuHFUT <3209310348@qq.com>
2026-06-21 15:45:47 +0800

    Auto-update tunnel: https://bitter-squids-jump.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://salty-schools-lay.loca.lt/api';
+  return 'https://bitter-squids-jump.loca.lt/api';
 }
 
 const request = axios.create({
