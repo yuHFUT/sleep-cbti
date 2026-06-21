5ecba5b470bad98a719474c2e746c94f59a2e435
yuHFUT <3209310348@qq.com>
2026-06-21 18:30:06 +0800

    Auto-update: https://huge-dolls-say.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://eleven-ads-heal.loca.lt/api';
+  return 'https://huge-dolls-say.loca.lt/api';
 }
 
 const request = axios.create({
