3922962cf4180ec909b74f2951adc90d2380a913
yuHFUT <3209310348@qq.com>
2026-06-21 18:37:30 +0800

    Auto-update: https://fruity-ways-arrive.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://olive-dolls-clap.loca.lt/api';
+  return 'https://fruity-ways-arrive.loca.lt/api';
 }
 
 const request = axios.create({
