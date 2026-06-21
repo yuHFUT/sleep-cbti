1ec730e85f72166d5cf7118e046e750889dffe5e
yuHFUT <3209310348@qq.com>
2026-06-21 18:37:08 +0800

    Auto-update: https://olive-dolls-clap.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://ready-boats-teach.loca.lt/api';
+  return 'https://olive-dolls-clap.loca.lt/api';
 }
 
 const request = axios.create({
