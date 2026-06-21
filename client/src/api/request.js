85b58a15d616c9e94e18ae40bebeabb685398c7b
yuHFUT <3209310348@qq.com>
2026-06-21 18:07:12 +0800

    Auto-update tunnel: https://stale-teeth-smoke.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://floppy-donuts-ask.loca.lt/api';
+  return 'https://stale-teeth-smoke.loca.lt/api';
 }
 
 const request = axios.create({
