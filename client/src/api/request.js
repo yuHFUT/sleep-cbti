1478cd6d7b7cdf77daaa5e1aaee57de0a9dffde2
yuHFUT <3209310348@qq.com>
2026-06-21 18:39:12 +0800

    Auto-update: https://silent-bottles-brush.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://plain-heads-peel.loca.lt/api';
+  return 'https://silent-bottles-brush.loca.lt/api';
 }
 
 const request = axios.create({
