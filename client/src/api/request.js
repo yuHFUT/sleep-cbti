13f74413679f50b68be021814b520e100fb3c469
yuHFUT <3209310348@qq.com>
2026-06-21 15:39:52 +0800

    Auto-update tunnel: https://salty-schools-lay.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://sleepcbti-7269.loca.lt/api';
+  return 'https://salty-schools-lay.loca.lt/api';
 }
 
 const request = axios.create({
