36cae89df60325f5057fe19ab95c2d8471f1abbe
yuHFUT <3209310348@qq.com>
2026-06-21 18:19:43 +0800

    Auto-update: https://all-apes-leave.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://famous-coats-turn.loca.lt/api';
+  return 'https://all-apes-leave.loca.lt/api';
 }
 
 const request = axios.create({
