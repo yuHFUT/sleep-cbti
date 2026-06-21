ef8a576082a327269e29ac99e6b6e82791e3574f
yuHFUT <3209310348@qq.com>
2026-06-21 18:20:46 +0800

    Auto-update: https://honest-doodles-jam.loca.lt

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -14,7 +14,7 @@ function getApiBase() {
   if (dynamic) return dynamic;
 
   // 回退：构建时预设的 API 地址
-  return 'https://rich-parrots-sing.loca.lt/api';
+  return 'https://honest-doodles-jam.loca.lt/api';
 }
 
 const request = axios.create({
